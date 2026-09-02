cf3a25d0f6a26edaf0d8604bcbc0dc6f909d572e
Abdul Saboor <157635054+AbdulSaboor-git@users.noreply.github.com>
2026-09-02 13:20:33 +0500

    a

--- .env.d
+++
@@ -1,9 +0,0 @@
-DB_HOST=http://localhost
-DB_PORT=5432
-DB_PASSWORD=abdsab
-DB_USERNAME=postgres
-DB_NAME=pet_store
-
-NEXT_PUBLIC_BASE_URL=http://localhost:3000
-
-DATABASE_URL=postgresql://postgres:abdsab@localhost:5432/pet_store
